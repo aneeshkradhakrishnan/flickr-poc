9020e083c5b0962424da0dbf58fbd53f0a482fb2
Aneesh Radhakrishnan <aneeshradhakrishnan@aneeshs-mbp.attlocal.net>
2019-10-16 16:56:38 -0400

    Random thumbnail in the recycler

--- app/src/main/java/com/poc/flickrdisplay/viewadapters/ImageListAdapter.kt
+++ app/src/main/java/com/poc/flickrdisplay/viewadapters/ImageListAdapter.kt
@@ -4,22 +4,41 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.poc.flickrdisplay.databinding.PhotoItemBinding
+import com.poc.flickrdisplay.databinding.PhotoThumbnailItemBinding
 import com.poc.flickrdisplay.model.Photo
 import com.poc.flickrdisplay.viewmodels.MainViewModel
+import java.util.*
 import javax.inject.Inject
 
 class ImageListAdapter @Inject constructor() : RecyclerView.Adapter<ImageViewHolder>() {
+
+    companion object {
+        const val MAIN_ROW = 1
+        const val THUMBNAIL = 2;
+    }
+
+    private var thumbnailRow: Int = 0
 
     private var data: List<Photo>? = null
     private var mainVm: MainViewModel? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
-        return ImageViewHolder(PhotoItemBinding.inflate(inflater, parent, false))
+        return when (viewType) {
+            THUMBNAIL -> ImageViewHolder(PhotoThumbnailItemBinding.inflate(inflater, parent, false))
+            else -> ImageViewHolder(PhotoItemBinding.inflate(inflater, parent, false))
+        }
     }
 
     override fun getItemCount(): Int {
         return data?.size ?: 0
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            thumbnailRow -> THUMBNAIL
+            else -> MAIN_ROW
+        }
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
@@ -29,6 +48,7 @@ class ImageListAdapter @Inject constructor() : RecyclerView.Adapter<ImageViewHol
     fun populateData(list: List<Photo>?, mainViewModel: MainViewModel?) {
         data = list
         mainVm = mainViewModel
+        thumbnailRow = Random().nextInt(itemCount)
         notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/poc/flickrdisplay/viewadapters/ImageViewHolder.kt
+++ app/src/main/java/com/poc/flickrdisplay/viewadapters/ImageViewHolder.kt
@@ -1,19 +1,35 @@
 package com.poc.flickrdisplay.viewadapters
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.poc.flickrdisplay.databinding.PhotoItemBinding
+import com.poc.flickrdisplay.databinding.PhotoThumbnailItemBinding
 import com.poc.flickrdisplay.model.Photo
 import com.poc.flickrdisplay.viewmodels.MainViewModel
 
-class ImageViewHolder(private val itemBinding: PhotoItemBinding) :
-        RecyclerView.ViewHolder(itemBinding.root) {
+class ImageViewHolder(view: View) :
+        RecyclerView.ViewHolder(view) {
+
+    var itemBinding: PhotoItemBinding? = null
+    var itemThumbnailItemBinding: PhotoThumbnailItemBinding? = null
+
+    constructor(itemBinding: PhotoItemBinding) : this(itemBinding.root) {
+        this.itemBinding = itemBinding
+    }
+
+    constructor(itemThumbnailItemBinding: PhotoThumbnailItemBinding) : this(itemThumbnailItemBinding.root) {
+        this.itemThumbnailItemBinding = itemThumbnailItemBinding
+    }
 
     init {
         itemBinding?.executePendingBindings()
+        itemThumbnailItemBinding?.executePendingBindings()
     }
 
     fun bind(photo: Photo, mainVm: MainViewModel?) {
         itemBinding?.photo = photo
         itemBinding?.mainVm = mainVm
+        itemThumbnailItemBinding?.photo = photo
+        itemThumbnailItemBinding?.mainVm = mainVm
     }
 }
